93982d6100faa3145e84676e3332e87bf9355fd3
Adedayo Omitayo <adedayo.omitayo@flowswift.com>
2014-07-31 02:25:51 +0100

    implemented publishChange method on resourceIO

--- test/spec/resourceio.js
+++ test/spec/resourceio.js
@@ -7,7 +7,7 @@ var supertest = require('supertest');
 var http = require('http');
 var SockjsClient = require('node-sockjs-client');
 
-describe('Api Express routing', function(){
+describe('Api Express resource io', function(){
 	var app, httpServer, ioClient;
 
 	before(function(done){
@@ -29,7 +29,22 @@ describe('Api Express routing', function(){
 			done();
 		});
 
+		apiexpress.action('createGreeting', function(command, done){
+			command.response = {greeting:command.params.body, statusCode: 201};
+			command.statusCode = 201;
+			done();
+		});
+
+		apiexpress.action('createGreeting').post(function(command, done){
+			command.api.resourceio.publishChange({topic:'/greetings', action:'create', data:command.params.body});
+			done();
+		});
+
 		app = require('express')();
+		var bodyParser = require('body-parser');
+		// parse application/json and application/x-www-form-urlencoded
+		app.use(bodyParser.urlencoded({extended:true}));
+		app.use(bodyParser.json());
 		app.use(apiexpress.http.router);
 		var port = testCase.port = process.env.PORT || 3030;
 		httpServer = http.createServer(app);
@@ -66,7 +81,6 @@ describe('Api Express routing', function(){
 	it('Broadcasts change notifications', function(done){
 		ioClient.onmessage = function(e){
 			var message = JSON.parse(e.data);
-			console.log(message);
 			expect(message).to.have.property('topic', 'sayHello');
 			expect(message).to.have.property('payload', 'hello world');
 			done();
@@ -75,6 +89,26 @@ describe('Api Express routing', function(){
 		supertest(app)
 		.get('/hello')
 		.expect(200)
+		.end(function(err){
+			if(err){done(err);}
+		});
+	});
+
+	it('Broadcasts change notifications for publishChange', function(done){
+		ioClient.onmessage = function(e){
+			var message = JSON.parse(e.data);
+			expect(message).to.have.property('topic', '/greetings');
+			expect(message).to.have.property('action', 'create');
+			expect(message).to.have.property('data');
+			expect(message.data).to.have.property('name', 'e k\'ale');
+			done();
+		};
+
+		supertest(app)
+		.post('/greetings')
+		.send({name:'e k\'ale'})
+		.set('Accept', 'application/json')
+		.expect(201)
 		.end(function(err){
 			if(err){done(err);}
 		});
